f25471eb098b8d6f3acfb9da12bb51d5943cef28
eapatton <eapatton91@gmail.com>
2020-05-03 14:37:07 -0700

    view routes started

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,8 +1,53 @@
 const express = require("express");
 const router = express.Router();
 
+//Home page view
 router.get("/", (req, res) => {
-  res.sendFile("/public/views/home.html", {
+  res.sendFile("views/home.html", {
+    root: __dirname + "/../",
+  });
+});
+
+//About page view
+//TODO:about.html
+router.get("/about", (req, res) => {
+  res.sendFile("views/about.html", {
+    root: __dirname + "/../",
+  });
+});
+
+//Mood index page view
+router.get("/moods", (req, res) => {
+  res.sendFile("views/moodsIndex.html", {
+    root: __dirname + "/../",
+  });
+});
+
+//Mood show page
+router.get("/moods/:moodId", (req, res) => {
+  res.sendFile("views/moodInfo.html", {
+    root: __dirname + "/../",
+  });
+});
+
+//register page view
+router.get("/register", (req, res) => {
+  res.sendFile("views/register.html", {
+    root: __dirname + "/../",
+  });
+});
+
+//---------------------------  POST ---------------------  //
+//new post page view
+router.get("/moods/:moodId/posts/new", (req, res) => {
+  res.sendFile("#", {
+    root: __dirname + "/../",
+  });
+});
+
+//edit post view
+router.get("/moods/:moodId/posts/:postId/edit", (req, res) => {
+  res.sendFile("views/postEdit.html", {
     root: __dirname + "/../",
   });
 });
